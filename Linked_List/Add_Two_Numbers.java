29447abf44ec50dc1558482c01a73d157485b53b
inzine <67794611+Inzamum-Ul-Haque@users.noreply.github.com>
2023-03-05 21:38:06 +0600

    Add files via upload

---
+++ Linked_List/Add_Two_Numbers.java
@@ -0,0 +1,44 @@
+package Linked_List;
+
+public class Add_Two_Numbers {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode();
+        ListNode result = dummy;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
+
+            int newVal = val1 + val2 + carry;
+            carry = newVal / 10;
+            newVal = newVal % 10;
+            result.next = new ListNode(newVal);
+
+            result = result.next;
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+
+        return dummy.next;
+    }
+}
---
+++ Linked_List/Reorder_List.java
@@ -0,0 +1,52 @@
+package Linked_List;
+
+public class Reorder_List {
+
+    public class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public void reorderList(ListNode head) {
+        //find the halves
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //reverse the second half
+        ListNode second = slow.next, prev = null;
+        slow.next = null;
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+
+        //merge the two halves
+        ListNode first = head;
+        second = prev;
+        while (second != null) {
+            ListNode tmp1 = first.next, tmp2 = second.next;
+            first.next = second;
+            second.next = tmp1;
+            first = tmp1;
+            second = tmp2;
+        }
+    }
+}
